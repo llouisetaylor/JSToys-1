20dfc6e3aa976e2e64ff31c00e334cf3a2d07e62
Guy Balaam <guybal@amazon.com>
2018-09-30 12:19:13 +0100

    Added touch event listeners

--- photoshop-js/draw.js
+++ photoshop-js/draw.js
@@ -40,8 +40,12 @@ let isDown = false;
 const lines = [];
 CONTAINER.addEventListener('mousedown', function(event) {
     isDown = true;
-    const hue = document.getElementById('colour').value;
-    lines.push(new Line([event.clientX, event.clientY], `hsl(${hue}, 100%, 50%)`));
+    lines.push(new Line([event.clientX, event.clientY]));
+});
+CONTAINER.addEventListener('touchstart', function(event) {
+    isDown = true;
+    const firstTouch = event.touches[0];
+    lines.push(new Line([firstTouch.clientX, firstTouch.clientY]));
 });
 CONTAINER.addEventListener('mousemove', function(event) {
     if (isDown == true) {
@@ -49,7 +53,17 @@ CONTAINER.addEventListener('mousemove', function(event) {
     }
     event.preventDefault();
 });
+CONTAINER.addEventListener('touchmove', function(event) {
+    if (isDown == true) {
+        const firstTouch = event.touches[0];
+        lines[lines.length - 1].addPoint([firstTouch.clientX, firstTouch.clientY]);
+    }
+    event.preventDefault();
+});
 document.addEventListener('mouseup', function() {
+    isDown = false;
+});
+document.addEventListener('touchend', function() {
     isDown = false;
 });
 
